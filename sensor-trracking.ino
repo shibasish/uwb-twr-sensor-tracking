8e2a7be37b295beb904e2f58d52d9a076b444e63
Shibasish Das <shibasish.das@1und1.de>
2023-08-12 12:12:20 +0200

    initial commit

---
+++ sensor-trracking.ino
@@ -0,0 +1,129 @@
+/* Wi-Fi STA Connect and Disconnect Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+   
+*/
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <SPI.h>
+#include "DW1000Ranging.h"
+
+#define ANCHOR_ADD "86:17:5B:D5:A9:9A:E2:9C"
+
+// connection pins
+const uint8_t PIN_RST = 9; // reset pin
+const uint8_t PIN_IRQ = 2; // irq pin
+const uint8_t PIN_SS = SS; 
+
+const char* ssid     = "Vodafone-E319";
+const char* password = "3eDBcatn4AQ88qEh";
+const char* mqtt_server = "192.168.0.80";
+
+int btnGPIO = 0;
+int btnState = false;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup()
+{
+    Serial.begin(300);
+
+    // SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
+    delay(10);
+
+    // Set GPIO0 Boot button as input
+    pinMode(btnGPIO, INPUT);
+
+    // We start by connecting to a WiFi network
+    // To debug, please enable Core Debug Level to Verbose
+
+    Serial.println();
+    Serial.print("[WiFi] Connecting to ");
+    Serial.println(ssid);
+    setup_wifi();
+
+    
+    Serial.println("Connecting to broker:");
+    client.setServer(mqtt_server, 1883);
+    Serial.println("Broker Connected");
+    
+    DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+    //define the sketch as anchor. It will be great to dynamically change the type of module
+    DW1000Ranging.attachNewRange(newRange);
+    DW1000Ranging.attachBlinkDevice(newBlink);
+    DW1000Ranging.attachInactiveDevice(inactiveDevice);
+    //Enable the filter to smooth the distance
+    //DW1000Ranging.useRangeFilter(true);
+  
+    //we start the module as an anchor
+    DW1000Ranging.startAsAnchor("82:17:5B:D5:A9:9A:E2:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("my-topic", "hello from device");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
+  }
+  DW1000Ranging.loop();
+  client.loop();
+  client.publish("my-topic", "{name: Hello}");
+  delay(1000);
+//    }
+}
+
+void newRange() {
+  Serial.print("from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+  Serial.print("\t Range: "); Serial.print(DW1000Ranging.getDistantDevice()->getRange()); Serial.print(" m");
+  Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
+}
+
+void newBlink(DW1000Device* device) {
+  Serial.print("blink; 1 device added ! -> ");
+  Serial.print(" short:");
+  Serial.println(device->getShortAddress(), HEX);
+}
+
+void inactiveDevice(DW1000Device* device) {
+  Serial.print("delete inactive device: ");
+  Serial.println(device->getShortAddress(), HEX);
+}
