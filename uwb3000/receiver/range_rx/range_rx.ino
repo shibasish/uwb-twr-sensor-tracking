f1583a8364b5cd9cad81297959cd61b1cd79994a
Shibasish Das <shibasish.das@1und1.de>
2023-08-14 22:32:37 +0200

    receive measurements from two anchors

--- uwb3000/receiver/range_rx/range_rx.ino
+++ uwb3000/receiver/range_rx/range_rx.ino
@@ -23,23 +23,23 @@
 
 /* Default communication configuration. We use default non-STS DW mode. */
 static dwt_config_t config = {
-    5,                /* Channel number. */
-    DWT_PLEN_128,     /* Preamble length. Used in TX only. */
-    DWT_PAC8,         /* Preamble acquisition chunk size. Used in RX only. */
-    9,                /* TX preamble code. Used in TX only. */
-    9,                /* RX preamble code. Used in RX only. */
-    1,                /* 0 to use standard 8 symbol SFD, 1 to use non-standard 8 symbol, 2 for non-standard 16 symbol SFD and 3 for 4z 8 symbol SDF type */
-    DWT_BR_6M8,       /* Data rate. */
-    DWT_PHRMODE_STD,  /* PHY header mode. */
-    DWT_PHRRATE_STD,  /* PHY header rate. */
-    (129 + 8 - 8),    /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
-    DWT_STS_MODE_OFF, /* STS disabled */
-    DWT_STS_LEN_64,   /* STS length see allowed values in Enum dwt_sts_lengths_e */
-    DWT_PDOA_M0       /* PDOA mode off */
+  5,                /* Channel number. */
+  DWT_PLEN_128,     /* Preamble length. Used in TX only. */
+  DWT_PAC8,         /* Preamble acquisition chunk size. Used in RX only. */
+  9,                /* TX preamble code. Used in TX only. */
+  9,                /* RX preamble code. Used in RX only. */
+  1,                /* 0 to use standard 8 symbol SFD, 1 to use non-standard 8 symbol, 2 for non-standard 16 symbol SFD and 3 for 4z 8 symbol SDF type */
+  DWT_BR_6M8,       /* Data rate. */
+  DWT_PHRMODE_STD,  /* PHY header mode. */
+  DWT_PHRRATE_STD,  /* PHY header rate. */
+  (129 + 8 - 8),    /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
+  DWT_STS_MODE_OFF, /* STS disabled */
+  DWT_STS_LEN_64,   /* STS length see allowed values in Enum dwt_sts_lengths_e */
+  DWT_PDOA_M0       /* PDOA mode off */
 };
 
-static uint8_t tx_poll_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, 'W', 'A', 'V', 'E', 0xE0, 0, 0};
-static uint8_t rx_resp_msg[] = {0x41, 0x88, 0, 0xCA, 0xDE, 'V', 'E', 'W', 'A', 0xE1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+static uint8_t tx_poll_msg[] = { 0x41, 0x88, 0, 0xCA, 0xDE, 'W', 'A', 'V', 'E', 0xE0, 0, 0 };
+static uint8_t rx_resp_msg[] = { 0x41, 0x88, 0, 0xCA, 0xDE, 'V', 'E', 'W', 'A', 0xE1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 static uint8_t frame_seq_nb = 0;
 static uint8_t rx_buffer[20];
 static uint32_t status_reg = 0;
@@ -54,9 +54,9 @@ extern dwt_txconfig_t txconfig_options;
 // connection pins
 // const uint8_t PIN_RST = 9; // reset pin
 // const uint8_t PIN_IRQ = 2; // irq pin
-// const uint8_t PIN_SS = SS; 
+// const uint8_t PIN_SS = SS;
 
-const char* ssid     = "Vodafone-E319";
+const char* ssid = "Vodafone-E319";
 const char* password = "3eDBcatn4AQ88qEh";
 const char* mqtt_server = "192.168.0.80";
 
@@ -66,15 +66,17 @@ const char* mqtt_server = "192.168.0.80";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void setup()
-{
+void setup() {
   initializeDW3000();
   setup_wifi();
   setup_mqtt_broker();
 }
 
-void loop()
-{
+void loop() {
+  int response_count = 0;
+  uint32_t frame_len;
+  float distances[2];
+
   /* Write frame data to DW IC and prepare transmission. See NOTE 7 below. */
   tx_poll_msg[ALL_MSG_SN_IDX] = frame_seq_nb;
   dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_TXFRS_BIT_MASK);
@@ -86,75 +88,107 @@ void loop()
   dwt_starttx(DWT_START_TX_IMMEDIATE | DWT_RESPONSE_EXPECTED);
 
   /* We assume that the transmission is achieved correctly, poll for reception of a frame or error/timeout. See NOTE 8 below. */
-  while (!((status_reg = dwt_read32bitreg(SYS_STATUS_ID)) & (SYS_STATUS_RXFCG_BIT_MASK | SYS_STATUS_ALL_RX_TO | SYS_STATUS_ALL_RX_ERR)))
-  {
-  };
+  // while (!((status_reg = dwt_read32bitreg(SYS_STATUS_ID)) & (SYS_STATUS_RXFCG_BIT_MASK | SYS_STATUS_ALL_RX_TO | SYS_STATUS_ALL_RX_ERR)))
+  // {
+  // };
 
-  /* Increment frame sequence number after transmission of the poll message (modulo 256). */
-  frame_seq_nb++;
+  while (response_count < 2) {
+    if (status_reg & SYS_STATUS_RXFCG_BIT_MASK) {
+      // uint32_t frame_len;
 
-  if (status_reg & SYS_STATUS_RXFCG_BIT_MASK)
-  {
-    uint32_t frame_len;
+      /* Clear good RX frame event in the DW IC status register. */
+      dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_RXFCG_BIT_MASK);
 
-    /* Clear good RX frame event in the DW IC status register. */
-    dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_RXFCG_BIT_MASK);
+      /* A frame has been received, read it into the local buffer. */
+      frame_len = dwt_read32bitreg(RX_FINFO_ID) & RXFLEN_MASK;
+      if (frame_len <= sizeof(rx_buffer)) {
+        dwt_readrxdata(rx_buffer, frame_len, 0);
 
-    /* A frame has been received, read it into the local buffer. */
-    frame_len = dwt_read32bitreg(RX_FINFO_ID) & RXFLEN_MASK;
-    if (frame_len <= sizeof(rx_buffer))
-    {
-      dwt_readrxdata(rx_buffer, frame_len, 0);
-
-      /* Check that the frame is the expected response from the companion "SS TWR responder" example.
+        /* Check that the frame is the expected response from the companion "SS TWR responder" example.
        * As the sequence number field of the frame is not relevant, it is cleared to simplify the validation of the frame. */
-      rx_buffer[ALL_MSG_SN_IDX] = 0;
-      if (memcmp(rx_buffer, rx_resp_msg, ALL_MSG_COMMON_LEN) == 0)
-      {
-        uint32_t poll_tx_ts, resp_rx_ts, poll_rx_ts, resp_tx_ts;
-        int32_t rtd_init, rtd_resp;
-        float clockOffsetRatio;
+        rx_buffer[ALL_MSG_SN_IDX] = 0;
+        if (memcmp(rx_buffer, rx_resp_msg, ALL_MSG_COMMON_LEN) == 0) {
+          uint32_t poll_tx_ts, resp_rx_ts, poll_rx_ts, resp_tx_ts;
+          int32_t rtd_init, rtd_resp;
+          float clockOffsetRatio;
 
-        /* Retrieve poll transmission and response reception timestamps. See NOTE 9 below. */
-        poll_tx_ts = dwt_readtxtimestamplo32();
-        resp_rx_ts = dwt_readrxtimestamplo32();
+          /* Retrieve poll transmission and response reception timestamps. See NOTE 9 below. */
+          poll_tx_ts = dwt_readtxtimestamplo32();
+          resp_rx_ts = dwt_readrxtimestamplo32();
 
-        /* Read carrier integrator value and calculate clock offset ratio. See NOTE 11 below. */
-        clockOffsetRatio = ((float)dwt_readclockoffset()) / (uint32_t)(1 << 26);
+          /* Read carrier integrator value and calculate clock offset ratio. See NOTE 11 below. */
+          clockOffsetRatio = ((float)dwt_readclockoffset()) / (uint32_t)(1 << 26);
 
-        /* Get timestamps embedded in response message. */
-        resp_msg_get_ts(&rx_buffer[RESP_MSG_POLL_RX_TS_IDX], &poll_rx_ts);
-        resp_msg_get_ts(&rx_buffer[RESP_MSG_RESP_TX_TS_IDX], &resp_tx_ts);
+          /* Get timestamps embedded in response message. */
+          resp_msg_get_ts(&rx_buffer[RESP_MSG_POLL_RX_TS_IDX], &poll_rx_ts);
+          resp_msg_get_ts(&rx_buffer[RESP_MSG_RESP_TX_TS_IDX], &resp_tx_ts);
 
-        /* Compute time of flight and distance, using clock offset ratio to correct for differing local and remote clock rates */
-        rtd_init = resp_rx_ts - poll_tx_ts;
-        rtd_resp = resp_tx_ts - poll_rx_ts;
+          /* Compute time of flight and distance, using clock offset ratio to correct for differing local and remote clock rates */
+          rtd_init = resp_rx_ts - poll_tx_ts;
+          rtd_resp = resp_tx_ts - poll_rx_ts;
 
-        tof = ((rtd_init - rtd_resp * (1 - clockOffsetRatio)) / 2.0) * DWT_TIME_UNITS;
-        distance = tof * SPEED_OF_LIGHT;
+          tof = ((rtd_init - rtd_resp * (1 - clockOffsetRatio)) / 2.0) * DWT_TIME_UNITS;
+          distance = tof * SPEED_OF_LIGHT;
 
-        /* Display computed distance on LCD. */
-        snprintf(dist_str, sizeof(dist_str), "DIST: %3.2f m", distance);
-        test_run_info((unsigned char *)dist_str);
+          distances[response_count] = distance;
+          response_count++;
+
+          /* Display computed distance on LCD. */
+          snprintf(dist_str, sizeof(dist_str), "DIST: %3.2f m", distance);
+          test_run_info((unsigned char*)dist_str);
+        }
       }
+    } else {
+      /* Clear RX error/timeout events in the DW IC status register. */
+      dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_ALL_RX_TO | SYS_STATUS_ALL_RX_ERR);
+    }
+
+    /* Increment frame sequence number after transmission of the poll message (modulo 256). */
+    frame_seq_nb++;
+
+    /* Process the distances[] array to compute the average */
+    if (response_count == 2) {
+      float average_distance = (distances[0] + distances[1]) / 2;
+
+      // Optionally, you can use a simple filter or thresholding method to discard any values that are too far from the average.
+      // This can be helpful in removing any spurious measurements.
+      float threshold = 0.2;  // For example, 20 cm. Adjust based on your needs.
+      if (fabs(distances[0] - distances[1]) > threshold) {
+        // If the difference between the two measurements exceeds the threshold,
+        // you might decide to discard this pair and maybe repeat the measurement.
+        // For now, we just consider the first measurement as the valid one.
+        average_distance = distances[0];
+      }
+
+      /* Display computed average distance on LCD. */
+      snprintf(dist_str, sizeof(dist_str), "AVG DIST: %3.2f m", average_distance);
+      test_run_info((unsigned char*)dist_str);
+
+      client.loop();
+
+      /* For the MQTT publish */
+      char b[12];
+      std::stringstream ss;
+
+      ss << average_distance;
+      ss >> b;
+      client.publish("my-topic", b);
+    } else {
+      // Handle cases where less than 2 valid responses were received.
+      // Maybe display an error or notify the user that the distance measurement was unsuccessful.
+      test_run_info((unsigned char*)"Measurement error!");
     }
   }
-  else
-  {
-    /* Clear RX error/timeout events in the DW IC status register. */
-    dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_ALL_RX_TO | SYS_STATUS_ALL_RX_ERR);
-  }
+  // client.loop();
 
-  client.loop();
+  // char b[12];
+  // std::stringstream ss;
 
-  char b[12];
-  std::stringstream ss;
+  // ss << distance;
+  // ss >> b;
+  // // cout << b << endl;
 
-  ss << distance;
-  ss >> b;
-  // cout << b << endl;
-
-  client.publish("my-topic", b);
+  // client.publish("my-topic", b);
 
   /* Execute a delay between ranging exchanges. */
   Sleep(RNG_DELAY_MS);
@@ -182,34 +216,33 @@ void setup_wifi() {
 }
 
 
-void setup_mqtt_broker(){
-    Serial.println("Connecting to broker:");
-    client.setServer(mqtt_server, 1883);
-    client.connect("ESP32Client");
-    Serial.println("Broker Connected");
+void setup_mqtt_broker() {
+  Serial.println("Connecting to broker:");
+  client.setServer(mqtt_server, 1883);
+  client.connect("ESP32Client");
+  Serial.println("Broker Connected");
 }
 
 // Placeholder function for initializing the DW3000 module
 void initializeDW3000() {
-    // Setup SPI, pins, and other initializations for the DW3000
-    // Refer to the DW3000 documentation for initialization details
+  // Setup SPI, pins, and other initializations for the DW3000
+  // Refer to the DW3000 documentation for initialization details
 
   UART_init();
 
   spiBegin(PIN_IRQ, PIN_RST);
   spiSelect(PIN_SS);
 
-  delay(2); // Time needed for DW3000 to start up (transition from INIT_RC to IDLE_RC, or could wait for SPIRDY event)
+  delay(2);  // Time needed for DW3000 to start up (transition from INIT_RC to IDLE_RC, or could wait for SPIRDY event)
 
-  while (!dwt_checkidlerc()) // Need to make sure DW IC is in IDLE_RC before proceeding
+  while (!dwt_checkidlerc())  // Need to make sure DW IC is in IDLE_RC before proceeding
   {
     UART_puts("IDLE FAILED\r\n");
     while (1)
       ;
   }
 
-  if (dwt_initialise(DWT_DW_INIT) == DWT_ERROR)
-  {
+  if (dwt_initialise(DWT_DW_INIT) == DWT_ERROR) {
     UART_puts("INIT FAILED\r\n");
     while (1)
       ;
@@ -219,7 +252,7 @@ void initializeDW3000() {
   dwt_setleds(DWT_LEDS_ENABLE | DWT_LEDS_INIT_BLINK);
 
   /* Configure DW IC. See NOTE 6 below. */
-  if (dwt_configure(&config)) // if the dwt_configure returns DWT_ERROR either the PLL or RX calibration has failed the host should reset the device
+  if (dwt_configure(&config))  // if the dwt_configure returns DWT_ERROR either the PLL or RX calibration has failed the host should reset the device
   {
     UART_puts("CONFIG FAILED\r\n");
     while (1)
@@ -245,141 +278,3 @@ void initializeDW3000() {
   Serial.println("Range RX");
   Serial.println("Setup over........");
 }
-
-
-// void reconnect() {
-//   while (!client.connected()) {
-//     Serial.print("Attempting MQTT connection...");
-//     if (client.connect("ESP32Client")) {
-//       Serial.println("connected");
-//       // Once connected, publish an announcement...
-//       // client.publish("my-topic", "hello from device");
-//     } else {
-//       Serial.print("failed, rc=");
-//       Serial.print(client.state());
-//       Serial.println(" try again in 5 seconds");
-//       delay(5000);
-//     }
-//   }
-// }
-
-// #include <WiFi.h>
-// #include <PubSubClient.h>
-// // #include <SPI.h>
-// // #include "DW1000Ranging.h"
-
-// #define ANCHOR_ADD "86:17:5B:D5:A9:9A:E2:9C"
-
-// // connection pins
-// const uint8_t PIN_RST = 9; // reset pin
-// const uint8_t PIN_IRQ = 2; // irq pin
-// const uint8_t PIN_SS = SS; 
-
-// const char* ssid     = "Vodafone-E319";
-// const char* password = "3eDBcatn4AQ88qEh";
-// const char* mqtt_server = "192.168.0.80";
-
-// int btnGPIO = 0;
-// int btnState = false;
-
-// WiFiClient espClient;
-// PubSubClient client(espClient);
-
-// void setup()
-// {
-//     Serial.begin(300);
-
-//     // SPI.begin(SPI_SCK, SPI_MISO, SPI_MOSI);
-//     delay(10);
-
-//     // Set GPIO0 Boot button as input
-//     pinMode(btnGPIO, INPUT);
-
-//     // We start by connecting to a WiFi network
-//     // To debug, please enable Core Debug Level to Verbose
-
-//     Serial.println();
-//     Serial.print("[WiFi] Connecting to ");
-//     Serial.println(ssid);
-//     setup_wifi();
-
-    
-//     Serial.println("Connecting to broker:");
-//     client.setServer(mqtt_server, 1883);
-//     Serial.println("Broker Connected");
-    
-//     // DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
-//     // //define the sketch as anchor. It will be great to dynamically change the type of module
-//     // DW1000Ranging.attachNewRange(newRange);
-//     // DW1000Ranging.attachBlinkDevice(newBlink);
-//     // DW1000Ranging.attachInactiveDevice(inactiveDevice);
-//     // //Enable the filter to smooth the distance
-//     // //DW1000Ranging.useRangeFilter(true);
-  
-//     // //we start the module as an anchor
-//     // DW1000Ranging.startAsAnchor("82:17:5B:D5:A9:9A:E2:9C", DW1000.MODE_LONGDATA_RANGE_ACCURACY);
-// }
-
-// void setup_wifi() {
-//   delay(10);
-//   Serial.println();
-//   Serial.print("Connecting to ");
-//   Serial.println(ssid);
-
-//   WiFi.begin(ssid, password);
-
-//   while (WiFi.status() != WL_CONNECTED) {
-//     delay(500);
-//     Serial.print(".");
-//   }
-
-//   Serial.println("");
-//   Serial.println("WiFi connected");
-//   Serial.println("IP address: ");
-//   Serial.println(WiFi.localIP());
-// }
-
-// void reconnect() {
-//   while (!client.connected()) {
-//     Serial.print("Attempting MQTT connection...");
-//     if (client.connect("ESP32Client")) {
-//       Serial.println("connected");
-//       // Once connected, publish an announcement...
-//       client.publish("my-topic", "hello from device");
-//     } else {
-//       Serial.print("failed, rc=");
-//       Serial.print(client.state());
-//       Serial.println(" try again in 5 seconds");
-//       delay(5000);
-//     }
-//   }
-// }
-
-// void loop()
-// {
-//   if (!client.connected()) {
-//     reconnect();
-//   }
-//   // DW1000Ranging.loop();
-//   client.loop();
-//   client.publish("my-topic", "{name: Hello}");
-//   delay(1000);
-// //    }
-// }
-
-// void newRange() {
-//   Serial.print("from: "); Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
-//   Serial.print("\t Range: "); Serial.print(DW1000Ranging.getDistantDevice()->getRange()); Serial.print(" m");
-//   Serial.print("\t RX power: "); Serial.print(DW1000Ranging.getDistantDevice()->getRXPower()); Serial.println(" dBm");
-// }
-
-// void newBlink(DW1000Device* device) {
-//   Serial.print("blink; 1 device added ! -> ");
-//   Serial.print(" short:");
-//   Serial.println(device->getShortAddress(), HEX);
-// }
-
-// void inactiveDevice(DW1000Device* device) {
-//   Serial.print("delete inactive device: ");
-//   Serial.println(device->getShortAddress(), HEX);
-// }
